75a9c91e79b9bf9650bc471917da1b971d66bcc0
Géraud MATHE <mail@geraudmathe.com>
2012-04-29 22:59:08 +0200

    first scaffold with models links and category

---
+++ app/models/link.rb
@@ -0,0 +1,3 @@
+class Link < ActiveRecord::Base
+  attr_accessible :name, :url
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railslinks::Application.config.secret_token = 'b04a172c96613b9c2ee755051d8cbffcd9fb96c46db568887c5531824117f87e5aeea778dcd8be3e0f0d69dafc505271753a47e3300d482f03d5aced7cada563'
