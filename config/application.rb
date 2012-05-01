e98c516f083fe3b025596579583eb66a97411d0f
Géraud MATHE <mail@geraudmathe.com>
2012-05-01 09:40:22 +0200

    added precompile false in application.rb

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module Railslinks
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    #to remove the assets precompile on heroku
+    config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
